2f61a2a5791f39fae92edc01b6545a6540d7a207
berdanasen <berdanasen@gmail.com>
2024-11-17 21:10:00 +0300

    add bootstrap and tailwind this project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import Test from './Test.js'
+import { Title } from './Components.js'
+import Bootstrap from './Bootstrap.js';
+import Tailwind from './Tailwind.js';
+
+import './tailwind.css'
+import styles from './App.module.css'
 
 function App() {
+  useEffect(() => {
+    console.log(styles);
+    if (process.env.NODE_ENV === 'production'){
+
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <img src="/logo192.png" alt=""/>
-      <img src={logo} alt=""/>
+    <div className={styles.App}>
+       <Title>{process.env.NODE_ENV}</Title>
+       <Title theme="dark">{process.env.NODE_ENV}</Title>
+       <p>{process.env.REACT_APP_API_URL}</p>
+       <Test/>
+       {process.env.NODE_ENV === 'production' && (
+        <>
+          <img src="/logo192.png" alt=""/>
+          <img src={logo} alt=""/>
+        </>
+       )}
+      <Bootstrap />
+      <Tailwind />
     </div>
   );
 }
